477917be11efa2268e509f8f37e98842ea4e132e
LLL <linnause@hotmail.com>
2019-09-11 16:31:13 -0400

    fixed version issue of eslint

---
+++ src/CreatAccount.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class CreatAccount extends React.Component {
+    static propTypes = {
+
+    }
+
+    render() {
+      return(
+        null
+        );
+    }
+}
+
+export default CreatAccount;
